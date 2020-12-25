28a8fff172fb19be949faffd7d4f2e645f53dad3
dariokl <darioklopic@gmail.com>
2020-12-25 14:55:53 +0100

    add

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ from app import create_app, db
 #Use to handle the command  flask db init / flask db migrate / flask db upgrade etc...
 from flask_migrate import Migrate
 
-app = create_app('testing')
+app = create_app('development')
 migrate = Migrate(app, db)
-
 
